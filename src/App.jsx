ffd2bdc6fcad4ca66abdca9317a91e90411dd674
Naufal A.N. <92589371+NaufalAndriantoNugraha@users.noreply.github.com>
2026-04-01 14:36:21 +0700

    Fitur: Tambah books section
    Untuk buku-buku yang ditampilkan berdasarkan novel star wars yang
    berlatar belakangkan era the old republic

--- src/App.jsx
+++ src/App.jsx
@@ -23,13 +23,13 @@ export default function App() {
             </a>
           </li>
           <li>
-            <a href="#" class="nav-link px-2">
-              Boook
+            <a href="#books" class="nav-link px-2">
+              Books
             </a>
           </li>
           <li>
             <a href="#" class="nav-link px-2">
-              Team
+              Teams
             </a>
           </li>
           <li>
@@ -47,7 +47,6 @@ export default function App() {
           </button>
         </div>
       </header>
-
       <div class="container col-xxl-8 px-4 py-5">
         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div class="col-10 col-sm-8 col-lg-6">
@@ -79,6 +78,87 @@ export default function App() {
               >
                 Rent the Book
               </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section class="py-5 text-center container" id="books">
+        <div class="row py-lg-5">
+          <div class="col-lg-8 col-md-8 mx-auto">
+            <h1 class="fw-semibold mb-4">The Old Republic Era Series</h1>
+            <p
+              class="lead text-body-secondary"
+              style={{ textAlign: "justify" }}
+            >
+              Dive into the ancient history of the Star Wars galaxy, thousands
+              of years before the rise of the Empire. The Old Republic Era
+              series explores the golden age of the Jedi, the fall of legendary
+              warriors, and the eternal struggle between the light and dark
+              sides of the Force. From the tragic story of Revan to the birth of
+              the Sith Empire, these novels bring to life an era of epic
+              battles, complex heroes, and timeless conflict.
+            </p>
+            <p>
+              <a href="#" class="btn btn-primary my-2 me-4">
+                Explore the series
+              </a>
+              <a href="#" class="btn btn-secondary my-2">
+                Browse all books
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
+      <div class="album py-5 bg-body-tertiary">
+        <div class="container">
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div class="col">
+              <div class="card shadow-sm">
+                <img
+                  src="https://static.wikia.nocookie.net/starwars/images/9/9b/Path-of-destruction.jpg/revision/latest?cb=20070719033113"
+                  style={{ borderRadius: "0.5rem" }}
+                />
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+                <img
+                  src="https://static.wikia.nocookie.net/starwars/images/1/13/RuleofTwo.jpg/revision/latest?cb=20081021130617"
+                  style={{ borderRadius: "0.5rem" }}
+                />
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+                <img
+                  src="https://static.wikia.nocookie.net/starwars/images/1/1c/Dynasty_of_Evil.jpg/revision/latest?cb=20090418194005"
+                  style={{ borderRadius: "0.5rem" }}
+                />
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+                <img
+                  src="https://static.wikia.nocookie.net/starwars/images/a/a3/AnnihilationCover.jpg/revision/latest?cb=20120601025557"
+                  style={{ borderRadius: "0.5rem" }}
+                />
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+                <img
+                  src="https://static.wikia.nocookie.net/starwars/images/c/cd/Swtor_deceived_cover.jpg/revision/latest?cb=20100715022755"
+                  style={{ borderRadius: "0.5rem" }}
+                />
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+                <img
+                  src="https://static.wikia.nocookie.net/starwars/images/6/63/TOR-Revan.jpg/revision/latest?cb=20110728211622"
+                  style={{ borderRadius: "0.5rem" }}
+                />
+              </div>
             </div>
           </div>
         </div>
